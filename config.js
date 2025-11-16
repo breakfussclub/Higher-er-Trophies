3462f57983cf43cc2c7c183d23d4cb3f07c5a84a
breakfussclub <104663894+breakfussclub@users.noreply.github.com>
2025-11-16 17:03:41 -0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+import { config } from 'dotenv';
+config();
+
+export default {
+  discord: {
+    token: process.env.DISCORD_TOKEN,
+    clientId: process.env.CLIENT_ID,
+    guildId: process.env.GUILD_ID
+  },
+  steam: {
+    apiKey: process.env.STEAM_API_KEY
+  },
+  psn: {
+    npsso: process.env.PSN_NPSSO
+  },
+  xbox: {
+    apiKey: process.env.OPENXBL_API_KEY  // Changed from clientId/clientSecret
+  }
+};
